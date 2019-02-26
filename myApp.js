0265414fb69c8965fb5de2ce02b9c229835255ab
Glitch (voracious-island) <none>
2019-02-26 02:01:19 +0000

    serve static assets

--- myApp.js
+++ myApp.js
@@ -15,16 +15,16 @@ console.log('hello world');
 
 /** 2) A first working Express Server */
 
+
+/** 3) Serve an HTML file */
+
 app.get('/', function(req, res) {
 res.sendFile(__dirname + '/views/index.html');
 })
 
-
-/** 3) Serve an HTML file */
-
-
 /** 4) Serve static assets  */
 
+app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 
